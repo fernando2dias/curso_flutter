9face76ebde05015a666308db9d04df7ae68bb47
fernando2dias <fernando3dias@gmail.com>
2021-05-01 18:54:35 -0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(Column(
+      children: <Widget>[
+        Text('Deliver features faster', textDirection: TextDirection.ltr,),
+        Text('Craft beautiful UIs', textDirection: TextDirection.ltr,),
+        Text('Craft beautiful UIs', textDirection: TextDirection.ltr,),
+      ],
+    ));
