5f3f2454e87fac8082505deb1a8f8fd88ca93dd8
Fernando Dias Motta <fernando.motta@fit-tecnologia.org.br>
2021-05-06 22:56:28 -0300

    Segunda aula - criamos mais widgets e extrair para classes.

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,55 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(Column(
-      children: <Widget>[
-        Text('Bem vendo ao Bytebank', textDirection: TextDirection.ltr,), //text
-        Text('Segundo text ', textDirection: TextDirection.ltr,), // text
-        Text('terceiro', textDirection: TextDirection.ltr,), //text
-        Column(
-          children: <Widget>[
-            Text('texto 4', textDirection: TextDirection.ltr,),
+void main() => runApp(
+  MaterialApp(
+    home: Scaffold(
+      body: ListaTransferencias(),
+      appBar: AppBar(title: Text('Transferências'),),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+      ),
+    ),
+  )
+);
 
-          ],
-
-        )
-
-
+class ListaTransferencias extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        ItemTransferencia(Transferencia(100.00, 1000)),
+        ItemTransferencia(Transferencia(250.00, 1000)),
+        ItemTransferencia(Transferencia(3000.00, 1000)),
       ],
-    ));
+    );
+  }
+}
 
+
+class ItemTransferencia extends StatelessWidget{
+
+  final Transferencia _transferencia;
+  ItemTransferencia(this._transferencia);
+
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: ListTile(
+        leading: Icon(Icons.monetization_on),
+        title: Text(_transferencia.valor.toString()),
+        subtitle: Text(_transferencia.numeroConta.toString()),
+      ),
+    );
+  }
+
+}
+
+class Transferencia {
+  final double valor;
+  final int numeroConta;
+  Transferencia(this.valor, this.numeroConta);
+
+
+}
