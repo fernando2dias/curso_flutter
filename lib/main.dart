4267784ea28103155478f12927a963838d14c8ea
Fernando Dias Motta <fernando.motta@fit-tecnologia.org.br>
2021-05-06 21:49:45 -0300

    Primeira aula

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,18 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(Column(
       children: <Widget>[
-        Text('Deliver features faster', textDirection: TextDirection.ltr,),
-        Text('Craft beautiful UIs', textDirection: TextDirection.ltr,),
-        Text('Craft beautiful UIs', textDirection: TextDirection.ltr,),
+        Text('Bem vendo ao Bytebank', textDirection: TextDirection.ltr,), //text
+        Text('Segundo text ', textDirection: TextDirection.ltr,), // text
+        Text('terceiro', textDirection: TextDirection.ltr,), //text
+        Column(
+          children: <Widget>[
+            Text('texto 4', textDirection: TextDirection.ltr,),
+
+          ],
+
+        )
+
+
       ],
     ));
+
